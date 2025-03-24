d1722026310877af4662afb6ecc72c1b43e9543f
J. B. de Vlam <jbdv2002@gmail.com>
2025-03-24 10:15:07 +0100

    Added the main text to LO page.

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function showBoxes() {
     // Hide the "Press me!" button
     document.getElementById('roundButton').style.display = 'none';
 
-    // Store the state in localStorage that the button was clicked
-    localStorage.setItem('buttonPressed', 'true');
+    // Store the state in sessionStorage that the button was pressed
+    sessionStorage.setItem('buttonPressed', 'true');
 
     // Show the background text when the button is pressed
     document.getElementById('backgroundText').style.display = 'block';
@@ -18,17 +18,17 @@ function showBoxes() {
     rectangles.forEach((rectangle, index) => {
         setTimeout(() => {
             rectangle.style.animation = 'showBoxes 0.5s forwards';
-        }, index * 200); // Delay each box's animation slightly
+        }, index * 200);
     });
 }
 
 // Check if the button has been pressed on page load
 window.onload = function() {
-    // If the button was previously pressed, show the boxes directly
-    if (localStorage.getItem('buttonPressed') === 'true') {
-        document.getElementById('roundButton').style.display = 'none'; // Hide the button
-        document.getElementById('backgroundText').style.display = 'block'; // Show the background text
-        document.getElementById('rectangleContainer').style.display = 'flex'; // Show the boxes
-        document.getElementById('rectangleContainer').style.opacity = 1; // Ensure the boxes are visible
+    // If the button was previously pressed (within the same session), show the boxes directly
+    if (sessionStorage.getItem('buttonPressed') === 'true') {
+        document.getElementById('roundButton').style.display = 'none';
+        document.getElementById('backgroundText').style.display = 'block';
+        document.getElementById('rectangleContainer').style.display = 'flex';
+        document.getElementById('rectangleContainer').style.opacity = 1;
     }
 };
